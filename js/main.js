eab003db809c971183952fcb6635c88507045911
Paolo Giovannini <giovannini13@gmail.com>
2023-02-24 10:14:32 +0100

    aggiunto simbolo euro

--- js/main.js
+++ js/main.js
@@ -9,11 +9,11 @@ let totCosto = km * prezzoKm;
 if (eta < 18){
     let sconto = totCosto / 100 * 20;
     totCosto = totCosto - sconto;
-    document.getElementById('costo').innerHTML=`Hai diritto allo sconto per gli under 18 del 20%, il prezzo è di ${totCosto.toFixed(2)}`;
+    document.getElementById('costo').innerHTML=`Hai diritto allo sconto per gli under 18 del 20%, il prezzo è di ${totCosto.toFixed(2)} €`;
 } else if (eta > 65){
     let sconto = totCosto / 100 * 40;
     totCosto = totCosto - sconto;
-    document.getElementById('costo').innerHTML=`Hai diritto allo sconto per gli over 65 del 40%, il prezzo è di ${totCosto.toFixed(2)}`;
+    document.getElementById('costo').innerHTML=`Hai diritto allo sconto per gli over 65 del 40%, il prezzo è di ${totCosto.toFixed(2)} €`;
 } else {
-    document.getElementById('costo').innerHTML=`Il prezzo è di ${totCosto.toFixed(2)}`;
+    document.getElementById('costo').innerHTML=`Il prezzo è di ${totCosto.toFixed(2)} €`;
 }
